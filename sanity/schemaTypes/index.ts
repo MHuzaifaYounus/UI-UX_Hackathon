0bfa82c34177321a1570f299cd7a6e94cc6a30ff
MHuzaifaYounus <huzaifayounus360@gmail.com>
2025-07-29 17:17:57 +0500

    simplify

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,6 @@
-import { type SchemaTypeDefinition } from 'sanity'
 import FoodItemSchema from './FoodItemSchema'
-import ChefsSchema from './ChefsSchema'
 import TestimonalsSchema from './TestimonalsSchema'
 
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [FoodItemSchema,ChefsSchema, TestimonalsSchema],
+export const schema = {
+  types: [FoodItemSchema, TestimonalsSchema],
 }
